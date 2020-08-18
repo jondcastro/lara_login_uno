2edcef32064419de0784bf4d26a23098244e237e
jonathan <jonathancastro2018@yahoo.com>
2020-08-18 13:36:27 -0400

    creando seeder para test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,44 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+
+        // $data = [            
+        //     [
+        //         'role' => '1'
+                
+        //     ],          
+        //     [
+        //        'role' => '2' 
+        //     ]
+        // ];
+
+        // DB::table('roles')->insert($data);
+
+    	$data = [
+    		[
+    			'first_name' => 'jonathan',
+    			'last_name' => 'castro',
+    			'email' => 'admin@hotmail.com',
+    			'password' => bcrypt('123'),
+    			'role_id' => 1    			
+    		],
+    		[
+    			'first_name' => 'maria',
+    			'last_name' => 'pacheco',
+    			'email' => 'maria@hotmail.com',
+    			'password' => bcrypt('123'),
+                'role_id' => 2 
+    		],    		
+    		[
+    			'first_name' => 'jose',
+    			'last_name' => 'martin',
+    			'email' => 'jose@hotmail.com',
+    			'password' => bcrypt('123'),
+                'role_id' => 2 
+    		]
+    	];
+
+    	DB::table('users')->insert($data);
     }
 }
